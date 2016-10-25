946d5681f7af24baadc991d5ebcf3f4c071ce656
Rui Lebre <ruilebre@ua.pt>
2016-10-25 11:03:13 +0100

    added first draft of topics screen

---
+++ app/src/main/java/com/ua/cm/project/unews/topics_fragments/LocalFragment.java
@@ -0,0 +1,21 @@
+package com.ua.cm.project.unews.topics_fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rui on 10/25/16.
+ */
+
+public class LocalFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
